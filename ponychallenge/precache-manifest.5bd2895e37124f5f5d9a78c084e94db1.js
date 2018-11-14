76401d3e6c562aed13e3508a48fe458a4322740d
JonasHoeiberg <jonashoeiberg@yahoo.dk>
2018-11-14 20:41:29 +0100

    Added pony challenge build

---
+++ ponychallenge/precache-manifest.5bd2895e37124f5f5d9a78c084e94db1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b65675798e21c27b764a",
+    "url": "/static/js/main.b6567579.chunk.js"
+  },
+  {
+    "revision": "702f419e2a73196afac3",
+    "url": "/static/js/1.702f419e.chunk.js"
+  },
+  {
+    "revision": "b65675798e21c27b764a",
+    "url": "/static/css/main.b50077b9.chunk.css"
+  },
+  {
+    "revision": "7f99b18f08d74f1f3dfd7bb5171af212",
+    "url": "/index.html"
+  }
+];
